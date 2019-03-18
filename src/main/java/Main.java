0deb0e7269be4b2de3798dca7b2eca6377dc6a27
06p204 <roman@orilinc.com>
2019-03-18 20:47:24 +0200

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.fit.pdfdom.PDFDomTree;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            PDDocument pdf = PDDocument.load(new File("/Users/ab/Desktop/test.pdf"));
+            Writer output = new PrintWriter("/Users/ab/Desktop/test.html", "utf-8");
+            new PDFDomTree().writeText(pdf, output);
+            output.close();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
